12418e758a1d5f00217691dd6c03ccf473d8897e
Stepan Ustyianovych <205234930+slipuk@users.noreply.github.com>
2026-07-05 22:45:42 +0300

    add url for redis connection

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+import redis
 from routes import router
 
 app = FastAPI()
 
 app.include_router(router=router)
 
+redis_client = redis.Redis(host="localhost", port=6379, db=0)
